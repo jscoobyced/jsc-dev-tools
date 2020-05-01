0d4d376911c1c44119e548a6af272c8fd2bc4129
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2020-05-01 16:54:22 +0700

    Fix build command.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker build -t jscdroiddev/jsc-dev-tools:latest .
-docker build -t jscdroiddev/jsc-net-dev-tools:latest Dockerfile.net
+docker build -t jscdroiddev/jsc-net-dev-tools:latest -f Dockerfile.net .
 
 docker push jscdroiddev/jsc-dev-tools:latest
 docker push jscdroiddev/jsc-net-dev-tools:latest

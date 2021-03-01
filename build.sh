4edb8a7dec74c3ee6572c2564946b8fbe5279ead
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-03-01 22:43:58 +0700

    Set latest

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#docker build -t jscdroiddev/jsc-dev-tools:latest .
-docker build -t jscdroiddev/jsc-net-dev-tools:3.1.201 -f Dockerfile.net .
+docker build -t jscdroiddev/jsc-dev-tools:latest .
+docker build -t jscdroiddev/jsc-net-dev-tools:latest -f Dockerfile.net .
 
-#docker push jscdroiddev/jsc-dev-tools:latest
-docker push jscdroiddev/jsc-net-dev-tools:3.1.201
+docker push jscdroiddev/jsc-dev-tools:latest
+docker push jscdroiddev/jsc-net-dev-tools:latest
